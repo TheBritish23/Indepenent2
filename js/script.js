34a0a078b290b137832558cb1e29f42adf835c89
Soro Cat <sorousha326@gmail.com>
2019-02-22 10:17:48 -0800

    add changes to js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 $(document).ready(function() {
-  var over21 = confirm("Are you over 21? Click OK for yes or Cancel for no.");
+  var over16 = confirm("Are you over 16? Click OK for yes or Cancel for no.");
+
+    if (over16) {
+      $('#drinks').show();
+    } else {
+      $('#under-16').show();
+    }
+  });
