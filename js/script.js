0949313e122124fbb6a28e346e5ab8d8436cff34
Soro Cat <sorousha326@gmail.com>
2019-02-28 11:02:13 -0800

    change css and js

--- js/script.js
+++ js/script.js
@@ -47,4 +47,24 @@ $(document).ready(function() {
         event.preventDefault();
       });
     });
+
+    $(document).ready(function() {
+      $("button#green").click(function() {
+        $("body").removeClass();
+        $("body").css("background-color", "green");
+        $("body").addClass("green-background");
+      });
+
+      $("button#yellow").click(function() {
+        $("body").removeClass();
+        $("body").css("background-color", "yellow");
+        $("body").addClass("yellow-background");
+      });
+
+      $("button#red").click(function() {
+        $("body").removeClass();
+        $("body").css("background-color", "red");
+        $("body").addClass("red-background");
+      });
+    });
   });
