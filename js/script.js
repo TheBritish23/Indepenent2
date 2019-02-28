3c644b37f5200dbd47b975ad453ba1657a74f749
Soro Cat <sorousha326@gmail.com>
2019-02-28 14:42:05 -0800

    add other changes

--- js/script.js
+++ js/script.js
@@ -27,26 +27,46 @@ $(document).ready(function() {
     });
   });
 
+  // Business logic not included because it will remain the same.
+
+  var add = function(number1, number2) {
+    return number1 + number2;
+  };
+
+  var subtract = function(number1, number2) {
+    return number1 - number2;
+  };
+
+  var multiply = function(number1, number2) {
+    return number1 * number2;
+  };
+
+  var divide = function(number1, number2) {
+    return number1 / number2;
+  };
+
+
+
+
   $(document).ready(function() {
-    $("form#lottery").submit(function(event) {
-        var age = parseInt($("input#age").val());
-        var gender = $("select#gender").val();
-
-        if (age) {
-          var cash = (100 - age) * 3;
-          if (gender === 'male' && age < 26) {
-            cash += 50;
-          }
-
-          $("#lottery").empty().append(quote);
-          $("#cash").show();
-        } else {
-          alert('Please enter your age.');
-        }
-
-        event.preventDefault();
-      });
+    $("form#calculator").submit(function() {
+      event.preventDefault();
+      var number1 = parseInt($("#input1").val());
+      var number2 = parseInt($("#input2").val());
+      var operator = $("input:radio[name=operator]:checked").val();
+      var result;
+      if (operator === "add") {
+        result = add(number1, number2);
+      } else if (operator === "subtract") {
+        result = subtract(number1, number2);
+      } else if (operator === "multiply") {
+        result = multiply(number1, number2);
+      } else if (operator === "divide") {
+        result = divide(number1, number2);
+      }
+      $("#output").text(result);
     });
+   });
 
     $(document).ready(function() {
       $("button#green").click(function() {
