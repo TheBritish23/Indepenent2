133ebbcefbe73ca13192f9e93855f6dfa058bf51
Soro Cat <sorousha326@gmail.com>
2019-03-22 16:16:46 -0700

    add changes in code and jscript

--- js/script.js
+++ js/script.js
@@ -10,37 +10,54 @@ $(document).ready(function() {
     var Q3 = $("input:radio[name=reliance]:checked").val();
     var Q4 = $("input:radio[name=coding]:checked").val();
     var Q5 = $("input:radio[name=computer]:checked").val();
-    console.log(Q1)
-    console.log(Q2)
-    console.log(Q3)
-    console.log(Q4)
-    console.log(Q5)
+
     if(Q1==="home"){
        csharp++;
     } else if(Q1==="office"){
        ruby++;
     } else if(Q1==="both"){
        java++;
+    }
     if(Q2==="modest"){
        php++;
     } else if(Q2==="medium"){
-       css++;
+       csharp++;
     } else if(Q2==="BIG(🎵I want it all!🎵)"){
        java++;
+    }
     if(Q3==="yes"){
        php++;
     } else if(Q3==="Depends on who they are."){
        csharp++;
-    } else if(Q3==="Hell no!!"){
-       css++;
+    } else if(Q3==="hell no!!"){
+       ruby++;
+    }
     if(Q4==="internal software"){
        csharp++;
     } else if(Q4==="user interface"){
        java++;
     } else if(Q4==="content management"){
        php++;
-                       
+    } else if(Q4==="interactive web applications") {
+       ruby++;
+    }
+    if(Q5==="pc"){
+      ruby++;
+    } else if(Q5==="mac"){
+      php++;
+    } else if (Q5==="linux"){
+      csharp++;
+    }
+    console.log(csharp)
+    console.log(php)
+    console.log(java)
+    console.log(ruby)
 
-      event.preventDefault();
-     });
-   });
+    $('results').append("<li>"(#:"+csharp+"</li>")
+    $('results').append("<li>"(java:"+java+"</li>")
+    $(".results").append("<li class='r-item'>C#"+csharp+"</li>")
+
+       event.preventDefault();
+      });
+    });
+  });
